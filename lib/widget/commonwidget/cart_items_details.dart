08b6cf1a94e96f8a820c0c180610c56f376f987f
SinsilaSheji <115135125+SinsilaSheji@users.noreply.github.com>
2024-03-16 17:19:51 +0530

    cart screen implementation

---
+++ lib/widget/commonwidget/cart_items_details.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:rawabi/utils/colors.dart';
+import 'package:rawabi/widget/commonwidget/reusable_text.dart';
+
+class CartItemDetails extends StatelessWidget {
+  final String imageName;
+
+  CartItemDetails({super.key, required this.imageName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.symmetric(horizontal: 15,vertical: 1),
+      //alignment: Alignment.center,
+      decoration:  BoxDecoration(
+        color: white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CircleAvatar(
+            radius: 25,
+            child: ClipOval(
+              child:Image.asset(
+                imageName,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          const Column(
+            children: [
+              ReusableText(
+                title: "Ajmi Special Pathiripodi 1 Kg",
+                size: 14,
+                weight: FontWeight.bold,
+              ),
+              ReusableText(
+                title: "QAR 10.50َ",
+                size: 14,
+                weight: FontWeight.bold,
+              ),
+
+            ],
+          ),
+
+          Spacer(),
+          CircleAvatar(
+            backgroundColor: pink,
+            radius: 15,
+            child: ClipOval(
+              child:Icon(Icons.remove_outlined)
+            ),
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          ReusableText(
+            title: "2",
+            size: 12,
+            weight: FontWeight.bold,
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          CircleAvatar(
+            backgroundColor: primaryColor,
+            radius: 15,
+            child: ClipOval(
+              child:Icon(Icons.add)
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
