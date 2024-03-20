d1b3f8f589a34e602cdf10e241deb944c128445c
Saneeb Salam <saneebsalam@gmail.com>
2024-03-20 12:51:57 +0300

    map current location

--- lib/screen/deliveryModeScreen.dart
+++ lib/screen/deliveryModeScreen.dart
@@ -36,32 +36,40 @@ class _DeliveryModeScreenState extends State<DeliveryModeScreen>
                   const SizedBox(
                     height: 50,
                   ),
-
-                  SizedBox(
-                    height: 45,
-                    child: ListTile(
-                      dense: true,
-                      visualDensity: const VisualDensity(vertical: -3),
-                      leading: InkWell(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Icon(
-                          Icons.arrow_back_ios,
-                          color: blackLight,
-                          size: 24,
+                  Container(
+                    height: 40,
+                    width: double.maxFinite,
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.only(top: 5, bottom: 5),
+                    child: Stack(
+                      children: [
+                        Center(
+                          child: ReusableText(
+                            title: "Delivery Mode".tr,
+                            size: 18,
+                            weight: FontWeight.bold,
+                            textAlign: TextAlign.left,
+                          ),
                         ),
-                      ),
-                      title: ReusableText(
-                        title: "Delivery Mode".tr,
-                        size: 18,
-                        weight: FontWeight.bold,
-                        textAlign: TextAlign.center,
-                      ),
+                        Positioned(
+                          left: 20,
+                          top: 0,
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Icon(
+                              Icons.arrow_back_ios,
+                              color: blackLight,
+                              size: 24,
+                            ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
                   const Divider(
-                    thickness: 2,
+                    thickness: 1,
                     color: lightGreyColor,
                   ),
                   SizedBox(
@@ -93,7 +101,6 @@ class _DeliveryModeScreenState extends State<DeliveryModeScreen>
                               textAlign: TextAlign.center,
                             ),
                           ),
-
                         ],
                       ),
                     ),
@@ -101,122 +108,29 @@ class _DeliveryModeScreenState extends State<DeliveryModeScreen>
                 ],
               ),
             ),
-
-        Expanded(
-
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 5,vertical: 5),
-            child: TabBarView(
-              physics: const NeverScrollableScrollPhysics(),
-              controller: _tabController,
-              children: [
-                const HomeDeliveryMapScreen(),
-                // Stack(
-                //   children: [
-                //     Container(
-                //       height: MediaQuery.of(context).size.height,
-                //       width: MediaQuery.of(context).size.width,
-                //       decoration: const BoxDecoration(
-                //         image: DecorationImage(
-                //           fit: BoxFit.cover,
-                //           image: AssetImage("assets/images/map.png"),
-                //         ),
-                //       ),
-                //       child: SafeArea(
-                //           child: Align(
-                //             alignment: Alignment.bottomCenter,
-                //             child: Container(
-                //               height: 210,
-                //               width: double.maxFinite,
-                //               padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
-                //               alignment: Alignment.bottomLeft,
-                //               decoration: BoxDecoration(
-                //                 color: white,
-                //                 borderRadius: BorderRadius.circular(20),
-                //               ),
-                //               child: Column(
-                //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //                 children: [
-                //
-                //                   Padding(
-                //                     padding: const EdgeInsets.only(top:18.0),
-                //                     child: Row(
-                //                       crossAxisAlignment: CrossAxisAlignment.start,
-                //                       children: [
-                //                         SvgPicture.asset(
-                //                           "assets/icons/location.svg",
-                //                           fit: BoxFit.contain,
-                //                           width: 25,
-                //                           height: 25,
-                //                         ),
-                //                         const SizedBox(
-                //                           width: 10,
-                //                         ),
-                //                         const Flexible(
-                //                           child: Text(
-                //                             'Al Wakra, Doha,\n Qatar.',
-                //                             softWrap: true,
-                //                             maxLines: 2,
-                //                             style: TextStyle(
-                //                               fontSize: 18,
-                //                               fontWeight: FontWeight.w600,
-                //                               fontFamily: "OpenSans",
-                //                               color: blackLight,
-                //                             ),
-                //                           ),
-                //                         ),
-                //                       ],
-                //                     ),
-                //                   ),
-                //
-                //                   ReusableButton(borderRadius:4.0,onTap: () {}, title: "Confirm Location"),
-                //                 ],
-                //               ),
-                //             ),
-                //           )),
-                //     ),
-                //     Container(
-                //       width: 380,
-                //       height: 45,
-                //       alignment: Alignment.center,
-                //       decoration: const BoxDecoration(
-                //           color: white,
-                //           borderRadius: BorderRadius.all(Radius.circular(4))),
-                //       margin: const EdgeInsets.all(10) ,
-                //       padding: const EdgeInsets.only(right: 10),
-                //       child:  TextField(
-                //         decoration: InputDecoration(
-                //           filled: true,
-                //           fillColor: white,
-                //           hintText: "Search Location".tr,
-                //           contentPadding: const EdgeInsets.only(left: 10),
-                //           prefixIcon: const Icon(Icons.search,color: blackLight,),
-                //           border: OutlineInputBorder(
-                //             borderRadius: BorderRadius.circular(4),
-                //             borderSide: BorderSide.none,
-                //           ),
-                //           focusedBorder: OutlineInputBorder(
-                //             borderRadius: BorderRadius.circular(4),
-                //             borderSide: BorderSide.none,
-                //           ),
-                //         ),
-                //       ),
-                //     )
-                //   ]
-                // ),
-                ListView.separated(
-                    padding: const EdgeInsets.all(0),
-                    shrinkWrap: true,
-                    // physics: const NeverScrollableScrollPhysics(),
-                    itemCount: 8,
-                    itemBuilder: (context, index) =>  const StoreTile(title: "Rawabi Al khor, T tower Building"),
-                    separatorBuilder: (BuildContext context, int index) =>
-                    const SizedBox(height: 5,)
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                child: TabBarView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  controller: _tabController,
+                  children: [
+                    const HomeDeliveryMapScreen(),
+                    ListView.separated(
+                        padding: const EdgeInsets.all(0),
+                        shrinkWrap: true,
+                        // physics: const NeverScrollableScrollPhysics(),
+                        itemCount: 8,
+                        itemBuilder: (context, index) => const StoreTile(
+                            title: "Rawabi Al khor, T tower Building"),
+                        separatorBuilder: (BuildContext context, int index) =>
+                            const SizedBox(
+                              height: 5,
+                            )),
+                  ],
                 ),
-
-              ],
+              ),
             ),
-          ),),
           ],
         ),
       ),
