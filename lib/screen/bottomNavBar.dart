dc5dad605d69b39cf7ee3509694698f214b8135f
SinsilaSheji <115135125+SinsilaSheji@users.noreply.github.com>
2024-03-23 16:05:45 +0530

    cart badge updated

--- lib/screen/bottomNavBar.dart
+++ lib/screen/bottomNavBar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:rawabi/screen/accountScreen.dart';
 import 'package:rawabi/screen/cartScreen.dart';
@@ -9,6 +10,7 @@ import 'package:rawabi/utils/colors.dart';
 import '../controller/cartController.dart';
 import '../widget/commonwidget/reusable_text.dart';
 import '../widget/commonwidget/svg_icon.dart';
+import 'package:badges/badges.dart' as badges;
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -22,7 +24,7 @@ class _BottomNavBarState extends State<BottomNavBar> {
   final cartController = Get.put(CartController());
   final List _pages = [
     /*HomeScreen(),*/
-    HomeNavigator(),
+    const HomeNavigator(),
     CategoryScreen(),
     OffersScreen(),
     CartScreen(),
@@ -34,14 +36,14 @@ class _BottomNavBarState extends State<BottomNavBar> {
       context: context,
       builder: (context) => AlertDialog(
         title:  Text("Rawabi".tr),
-        content:  Text('Wish to exit from App ? '),
+        content:  const Text('Wish to exit from App ? '),
         actions: <Widget>[
           TextButton(
             onPressed: () => Navigator.of(context).pop(false), //<-- SEE HERE
             child:  Text('No'.tr),
           ),
           TextButton(
-            onPressed: () => Navigator.of(context).pop(true), // <-- SEE HERE
+            onPressed: () => SystemNavigator.pop(), // <-- SEE HERE
             child:  Text('Yes'.tr),
           ),
         ],
@@ -79,157 +81,171 @@ class _BottomNavBarState extends State<BottomNavBar> {
             decoration: const BoxDecoration(
               color: Colors.white,
             ),
-            child: Stack(
+            child: SizedBox(
+              height: 90,
+              child: Row(
+                //crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                SizedBox(
-                  height: 90,
-                  child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                        onTap: () {
-                          setState(() {
-                            _currentIndex = 0;
-                          });
-                        },
-                        child: SizedBox(
-                          height: 40,
-                          child: Column(
-                            children: [
-                              SvgIcon(
-                                image: "assets/icons/blue_home.svg",
-                                height: 20,
-                                color:
-                                    _currentIndex == 0 ? primaryColor : Colors.black,
-                              ),
-                              ReusableText(
-                                title: "Home".tr,
-                                color:
-                                    _currentIndex == 0 ? primaryColor : Colors.black,
-                                size: 12,
-                              )
-                            ],
+                InkWell(
+                    onTap: () {
+                      setState(() {
+                        _currentIndex = 0;
+                      });
+                    },
+                    child: SizedBox(
+                      height: 40,
+                      child: Column(
+                        children: [
+                          SvgIcon(
+                            image: "assets/icons/blue_home.svg",
+                            height: 20,
+                            color:
+                                _currentIndex == 0 ? primaryColor : Colors.black,
                           ),
-                        )),
-                    InkWell(
-                        onTap: () {
-                          setState(() {
-                            _currentIndex = 1;
-                          });
-                        },
-                        child: SizedBox(
-                          height: 40,
-                          child: Column(
-                            children: [
-                              SvgIcon(
-                                image: "assets/icons/explore.svg",
-                                height: 20,
-                                color:
-                                    _currentIndex == 1 ? primaryColor : Colors.black,
-                              ),
-                              ReusableText(
-                                title: "Explore".tr,
-                                color:
-                                    _currentIndex == 1 ? primaryColor : Colors.black,
-                                size: 12,
-                              )
-                            ],
+                          ReusableText(
+                            title: "Home".tr,
+                            color:
+                                _currentIndex == 0 ? primaryColor : Colors.black,
+                            size: 12,
+                          )
+                        ],
+                      ),
+                    )),
+                InkWell(
+                    onTap: () {
+                      setState(() {
+                        _currentIndex = 1;
+                      });
+                    },
+                    child: SizedBox(
+                      height: 40,
+                      child: Column(
+                        children: [
+                          SvgIcon(
+                            image: "assets/icons/explore.svg",
+                            height: 20,
+                            color:
+                                _currentIndex == 1 ? primaryColor : Colors.black,
                           ),
-                        )),
-                    InkWell(
-                        onTap: () {
-                          setState(() {
-                            _currentIndex = 2;
-                          });
-                        },
-                        child: SizedBox(
-                          height: 40,
-                          child: Column(
-                            children: [
-                              SvgIcon(
-                                image: "assets/icons/gift.svg",
-                                height: 20,
-                                color:
-                                    _currentIndex == 2 ? primaryColor : Colors.black,
-                              ),
-                              ReusableText(
-                                title: "Offers".tr,
-                                color:
-                                    _currentIndex == 2 ? primaryColor : Colors.black,
-                                size: 12,
-                              )
-                            ],
+                          ReusableText(
+                            title: "Explore".tr,
+                            color:
+                                _currentIndex == 1 ? primaryColor : Colors.black,
+                            size: 12,
+                          )
+                        ],
+                      ),
+                    )),
+                InkWell(
+                    onTap: () {
+                      setState(() {
+                        _currentIndex = 2;
+                      });
+                    },
+                    child: SizedBox(
+                      height: 40,
+                      child: Column(
+                        children: [
+                          SvgIcon(
+                            image: "assets/icons/gift.svg",
+                            height: 20,
+                            color:
+                                _currentIndex == 2 ? primaryColor : Colors.black,
                           ),
-                        )),
-                    InkWell(
-                        onTap: () {
-                          setState(() {
-                            _currentIndex = 3;
-                          });
-                        },
-                        child: SizedBox(
-                          height: 40,
-                          child: Column(
-                            children: [
-                              SvgIcon(
-                                image: "assets/icons/cart.svg",
-                                height: 20,
-                                color:
-                                _currentIndex == 3 ? primaryColor : Colors.black,
-                              ),
-                              ReusableText(
-                                title: "Cart".tr,
-                                color:
-                                _currentIndex == 3 ? primaryColor : Colors.black,
-                                size: 12,
-                              )
-                            ],
-                          ),
-                        )),
-                    InkWell(
-                        onTap: () {
-                          setState(() {
-                            _currentIndex = 4;
-                          });
-                        },
-                        child: SizedBox(
-                          height: 40,
-                          child: Column(
-                            children: [
-                              SvgIcon(
-                                image: "assets/icons/account.svg",
-                                height: 20,
-                                color:
-                                    _currentIndex == 4 ? primaryColor : Colors.black,
-                              ),
-                              ReusableText(
-                                title: "Account".tr,
-                                color:
-                                    _currentIndex == 4 ? primaryColor : Colors.black,
-                                size: 12,
-                              )
-                            ],
-                          ),
-                        )),
-                  ],
-                                ),
-                ),
-                Positioned(
-
-                  right: 75,
-                  bottom: 60,
-                  child: CircleAvatar(
-                    backgroundColor: primaryColor,
-                    radius: 10,
-                    child: Obx(()=>Text(cartController.itemCount.value.toString(),
-                      style: const TextStyle(
-                        fontSize: 10,
-                        color: white,
-                      ),),
-                    ),
+                          ReusableText(
+                            title: "Offers".tr,
+                            color:
+                                _currentIndex == 2 ? primaryColor : Colors.black,
+                            size: 12,
+                          )
+                        ],
+                      ),
+                    )),
+                badges.Badge(
+                  position: badges.BadgePosition.topEnd(top: -10, end: -12),
+                  showBadge: true,
+                  ignorePointer: false,
+                  onTap: () {
+                    setState(() {
+                    _currentIndex = 3;
+                  });
+                    },
+                  badgeContent:
+                  Obx(()=>Text(cartController.itemCount.value.toString(),
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: white,
+                    ),),),
+                  badgeAnimation: const badges.BadgeAnimation.scale(
+                    animationDuration: Duration(seconds: 1),
+                    colorChangeAnimationDuration: Duration(seconds: 1),
+                    loopAnimation: false,
+                    curve: Curves.fastOutSlowIn,
+                    colorChangeAnimationCurve: Curves.easeInCubic,
                   ),
+                  badgeStyle: badges.BadgeStyle(
+                    shape: badges.BadgeShape.circle,
+                    badgeColor: primaryColor,
+                    padding: const EdgeInsets.all(8),
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.white, width: 2),
+                    elevation: 0,
+                  ),
+
+                  child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          _currentIndex = 3;
+                        });
+                      },
+                      child: SizedBox(
+                        height: 40,
+                        child: Column(
+                          children: [
+                            SvgIcon(
+                              image: "assets/icons/cart.svg",
+                              height: 20,
+                              color:
+                              _currentIndex == 3 ? primaryColor : Colors.black,
+                            ),
+                            ReusableText(
+                              title: "Cart".tr,
+                              color:
+                              _currentIndex == 3 ? primaryColor : Colors.black,
+                              size: 12,
+                            )
+                          ],
+                        ),
+                      )),
                 ),
-              ]
+                InkWell(
+                    onTap: () {
+                      setState(() {
+                        _currentIndex = 4;
+                      });
+                    },
+                    child: SizedBox(
+                      height: 40,
+                      child: Column(
+                        children: [
+                          SvgIcon(
+                            image: "assets/icons/account.svg",
+                            height: 20,
+                            color:
+                                _currentIndex == 4 ? primaryColor : Colors.black,
+                          ),
+                          ReusableText(
+                            title: "Account".tr,
+                            color:
+                                _currentIndex == 4 ? primaryColor : Colors.black,
+                            size: 12,
+                          )
+                        ],
+                      ),
+                    )),
+              ],
+                            ),
             ),
           ),
         ),
