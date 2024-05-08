45855ad0da8e6290ef26cbd26423268bd9f82505
SinsilaSheji <115135125+SinsilaSheji@users.noreply.github.com>
2024-05-08 13:14:59 +0530

    selectSlot implemented

--- lib/screen/home/selectSlotScreen.dart
+++ lib/screen/home/selectSlotScreen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:rawabi/utils/colors.dart';
 import 'package:intl/intl.dart';
-import 'package:rawabi/utils/constants.dart';
 import '../../controller/slotController.dart';
 import '../../widget/Commonwidget/reusable_text.dart';
 
